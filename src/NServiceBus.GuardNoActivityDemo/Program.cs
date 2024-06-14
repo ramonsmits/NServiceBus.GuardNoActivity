836d1d7c209866c79f9fadcf46177f0e6ba5f37a
Ramon Smits <ramon.smits@gmail.com>
2024-06-14 13:41:52 +0200

    Improved demo

--- src/NServiceBus.GuardNoActivityDemo/Program.cs
+++ src/NServiceBus.GuardNoActivityDemo/Program.cs
@@ -1,6 +1,5 @@
-﻿using NServiceBus;
-
-var cfg = new EndpointConfiguration("demo");
+﻿var cfg = new EndpointConfiguration("demo");
+cfg.UseSerialization(new SystemJsonSerializer());
 cfg.UseTransport(new LearningTransport());
 cfg.EnableInstallers();
 cfg.RaiseCriticalWhenNoActivity(TimeSpan.FromSeconds(3));
@@ -8,10 +7,25 @@ cfg.Recoverability().Immediate(c => c.NumberOfRetries(0));
 cfg.Recoverability().Delayed(c => c.NumberOfRetries(0));
 
 var instance = await Endpoint.Start(cfg);
+
+for (int i = 0; i < 3; i++)
+{
+    await instance.SendLocal(new MyMessage());
+    await Task.Delay(TimeSpan.FromSeconds(2));
+}
+
 await Task.Delay(TimeSpan.FromSeconds(10));
 
 await instance.Stop();
 
 class MyMessage : IMessage
 {
+}
+
+class MyHandler : IHandleMessages<MyMessage>
+{
+    public Task Handle(MyMessage message, IMessageHandlerContext context)
+    {
+        return Console.Out.WriteLineAsync($"Received message at {DateTime.Now}");
+    }
 }
